0005cd3a5caa6dfd1e9c4d8f23d7a024679e8369
notlarry <notlarry@mcp.r00tw4rz.org>
2018-01-07 20:04:46 -0500

    cleaned up debug echo and implemented dig to get IP

--- getmyip.sh
+++ getmyip.sh
@@ -15,18 +15,18 @@
 source /etc/getmyip/getmyip.conf        # configuration settings
 
 # get the current IP from dd-wrt router @192.168.1.1 and place it in $public_ip
-public_ip=`lynx -dump http://192.168.1.1 | grep WAN\ IP: | awk '{print $3}'`
+# changed this at the suggestion of wilpig @ https://github.com/wilpig
+public_ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
 
 # check $public_ip against $last_public_ip.  If they are not the same put the
 # $public_ip value into the ip file and send the new ip address to #r00tw4rz at
 # notlarry.slack.com
 if [ "$public_ip" != "$last_public_ip" ]; then 
-    echo $public_ip
     sed -i "s/^last_public_ip.*/last_public_ip=\"${public_ip}\"/" /etc/getmyip/getmyip.conf    
 
     # this would be nice if I could do it #curl $curl_cmd                                               # Slack new IP
     # but this works for now
-    curl -X POST --data-urlencode "payload={\"channel\":\"$channel\", \"username\": \"$username\", \"text\": \"r00tw4rz.org has a new IP @ $public_ip.\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/$hook
+    curl -X POST --data-urlencode "payload={\"challen\":\"$channel\", \"username\": \"$username\", \"text\": \"r00tw4rz.org has a new IP @ $public_ip.\", \"icon_emoji\": \":ghost:\"}" https://hooks.slack.com/services/$hook
 
 fi
 
